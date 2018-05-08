b967a71165b4e23438037a865b048f87b06ce4a1
lucialiu <lucialiux3@gmail.com>
2018-05-08 13:51:29 -0400

    Added Lucia's basic stuff
    Still need to format and style.. I will also be adding the elephant death count at the bottom of mine.

---
+++ Lucia/piechart.js
@@ -0,0 +1,226 @@
+var yr = 2003;
+
+var numPies = 5;  // total west central east south
+var regions = ["total","west","central","east","south"];
+
+var radius = 72;
+
+// colors
+var color = d3.scaleOrdinal(["#71353e", "#33222a"]);
+
+var pie = d3.pie()
+    .sort(null);
+
+var path = d3.arc()   // circle
+    .outerRadius(radius) // radius - x where x is margin
+    .innerRadius(36);  // center circle
+
+var label = d3.arc()  // label locations
+    .outerRadius(radius - 18)   // should be the same for inner and outer
+    .innerRadius(radius - 18);
+
+
+var dotWrapper = document.getElementById("dot-wrapper");
+
+var legend = d3.select("#legend-wrapper");
+
+
+
+updateData(yr);
+
+function updateData(year) {
+
+  d3.selectAll(".piechart").remove();
+  //d3.selectAll(".dot").remove();
+
+  d3.csv("Lucia/data/" + "PIKE_africa/" + yr + ".csv", function(d) {
+    // console.log(d);
+    d.total= +d.total;
+    d.west = +d.west;
+    d.central = +d.central;
+    d.east = +d.east;
+    d.south = +d.south;
+    d.totalcarc = +d.totalcarc;
+    d.westcarc = +d.westcarc;
+    d.centralcarc = +d.centralcarc;
+    d.eastcarc = +d.eastcarc;
+    d.southcarc = +d.southcarc;
+    return d;
+  }, function(error, data) {
+    if (error) throw error;
+
+    // console.log(data);
+    // console.log(data[0]["total"]);
+
+    for (i = 0; i < numPies; i++) {   // make 5 pie charts
+      var svg = d3.select("#piechart-wrapper")
+          .data(data)
+        .append("svg")
+          .attr("width", 160)
+          .attr("height", 160)
+          .attr("class","piechart"),
+        g = svg.append("g").attr("transform", "translate(" + 80 + "," + 80 + ")");
+      svg.append("text")
+        .attr("transform", "translate(" + 80 + "," + 80 + ")")
+        .attr("dy", "0.5em")
+        .attr("class","region")
+        .text(regions[i])
+        .style("fill","#7d96a0");
+
+      if (i == 0) {
+        pie.value(function(d) { return d.total; });
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+        arc.append("path")
+          .transition()
+          .duration(200)
+          .attr("d", path)
+          // .attr("stroke", "#7d96a0")
+          .attr("fill", function(d) { return color(d.data.total); });
+        arc.append("text")
+          .transition()
+          .duration(200)
+          .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .attr("fill", "#ce974b")
+          .attr("opacity", ".5")
+          .text(function(d) { return (round(d.data.total*100, 0)) + "%"; })
+
+      } else if (i == 1) {
+        pie.value(function(d) { return d.west; });
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+        arc.append("path")
+          .transition()
+          .duration(200)
+          .attr("d", path)
+          .attr("fill", function(d) { return color(d.data.west); })
+        arc.append("text")
+          .transition()
+          .duration(200)
+          .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .attr("fill", "#ce974b")
+          .attr("opacity", ".5")
+          .text(function(d) { return (round(d.data.west*100, 0)) + "%"; });
+
+      } else if (i == 2) {
+        pie.value(function(d) { return d.central; });
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+        arc.append("path")
+          .transition()
+          .duration(200)
+          .attr("d", path)
+          .attr("fill", function(d) { return color(d.data.central); });
+        arc.append("text")
+          .transition()
+          .duration(200)
+          .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .attr("fill", "#ce974b")
+          .attr("opacity", ".5")
+          .text(function(d) { return (round(d.data.central*100, 0)) + "%"; });
+
+      } else if (i == 3) {
+        pie.value(function(d) { return d.east; });
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+        arc.append("path")
+          .transition()
+          .duration(200)
+          .attr("d", path)
+          .attr("fill", function(d) { return color(d.data.east); });
+        arc.append("text")
+          .transition()
+          .duration(200)
+          .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .attr("fill", "#ce974b")
+          .attr("opacity", ".5")
+          .text(function(d) { return (round(d.data.east*100, 0)) + "%"; });
+
+      } else if (i == 4) {
+        pie.value(function(d) { return d.south; });
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+        arc.append("path")
+          .transition()
+          .duration(200)
+          .attr("d", path)
+          .attr("fill", function(d) { return color(d.data.south); });
+        arc.append("text")
+          .transition()
+          .duration(200)
+          .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .attr("fill", "#ce974b")
+          .attr("opacity", ".5")
+          .text(function(d) { return (round(d.data.south*100, 0)) + "%"; });
+      }
+    }
+
+    // add elephant dots
+    // console.log(data[0]["totalcarc"]);  // total
+    // console.log(data[1]["totalcarc"]);  // illegal
+    var numTotal = data[0]["totalcarc"];
+    var numIllegal = data[1]["totalcarc"];
+    var numLegal = numTotal - numIllegal;
+    console.log("height: " + Math.ceil(numTotal/50)*16);
+    console.log("height Illegal: " + Math.ceil(numIllegal/50)*16);
+    console.log("illegal: " + numIllegal);
+    console.log("total: " + numTotal);
+
+    // DOTS
+    // for (i = 0; i < numIllegal; i++) {
+    //   dotWrapper.innerHTML += "<img class='dot' src='images/elephant-illegal.svg'/>";
+    // }
+    // for (i = 0; i < numLegal; i++) {
+    //   dotWrapper.innerHTML += "<img class='dot' src='images/elephant-legal.svg'/>";
+    // }
+
+    // <svg height="100" width="100">
+    //   <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+    // </svg>
+    // 2 ELEPHANTS
+    // var maxWidth = 400;
+    // var illegalSize = sizeElephant(maxWidth, numTotal, numIllegal);
+    // var legalSize = sizeElephant(maxWidth, numTotal, numLegal);
+    // dotWrapper.innerHTML += "<img class='dot' width='" + illegalSize + "'' src='images/elephant-illegal.svg'/>";
+    // dotWrapper.innerHTML += "<img class='dot' width='" + legalSize + "'' src='images/elephant-legal.svg'/>";
+  });
+}
+
+// function svgcircle(h, w, cx, cy, r, stroke, strokewidth, fill) {
+//   var circlehtml = "<circle cx='" + cx + "' cy='" + cy + "' r='" + r + "' stroke=" + stroke + " stroke-width='" + strokewidth + "' fill=" + fill + ">";
+//   var svghtml = "<svg class='dot' height='" + h + "' width='" + w +"'>" + circlehtml + "</svg>";
+//   return svghtml;
+// }
+
+// function to round number to certain precision
+function round(number, precision) {
+  var shift = function (number, precision, reverseShift) {
+    if (reverseShift) {
+      precision = -precision;
+    }  
+    var numArray = ("" + number).split("e");
+    return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+  };
+  return shift(Math.round(shift(number, precision, false)), precision, true);
+}
+
+function sizeElephant(maxWidth, total, select) {
+  var proportion = select/total;
+  var newWidth = proportion * maxWidth;
+  return newWidth;
+}
---
+++ Lucia/slider.js
@@ -0,0 +1,90 @@
+var margin = {top: 20, right: 50, bottom: 20, left: 50},
+    width = 800 - margin.left - margin.right,
+    height = 120 - margin.bottom - margin.top;
+
+var x = d3.scale.linear()
+    .domain([2003, 2016]) // SLIDER RANGE
+    .range([0, width])
+    .clamp(true);
+
+var brush = d3.svg.brush()
+    .x(x)
+    .extent([0, 0])
+    .on("brush", brushed)
+    .on("brushend", brushend);
+
+var svg = d3.select("#slider-wrapper").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .attr("class", "x Luciaaxis")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .call(d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(function(d) { return d; })  // SLIDER FORMAT
+      .tickSize(0)
+      .tickPadding(18))
+      .style("fill","#7d96a0")
+  .select(".domain")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "halo");
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .call(brush);
+
+slider.selectAll(".extent,.resize")
+    .remove();
+
+slider.select(".background")
+    .attr("height", height);
+
+var handle = slider.append("circle")
+    .attr("class", "handle")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .attr("r", 8);  // slider circle radius
+
+slider
+    .call(brush.event)
+  .transition() // gratuitous intro!
+    .duration(750)
+    .call(brush.extent([2003, 2003])) // MUST BE FIRST VALUE I THINK
+    .call(brush.event);
+
+function brushed() {
+  var value = brush.extent()[0];
+
+  if (d3.event.sourceEvent) { // not a programmatic event
+    value = x.invert(d3.mouse(this)[0]);
+    brush.extent([value, value]);
+  }
+
+  handle.attr("cx", x(value));
+}
+
+function brushend() {
+   if (!d3.event.sourceEvent) {
+     return; // only transition after input
+   }
+
+   var value = brush.extent()[0];                    
+   brush.extent([value, value]);    // value = value on slider on mouse up
+   
+   d3.select(this)
+     .transition()
+     .duration(0)
+     .call(brush.event);                           
+  
+   d3.select(this)
+     .transition()
+     .call(brush.extent(brush.extent().map(function(d) { return d3.round(d, 0); }))) // SNAPS TO DIGIT at number 0
+     .call(brush.event);
+
+    yr = round(value,0);
+
+    updateData(yr);
+ }
