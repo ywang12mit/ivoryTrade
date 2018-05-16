81aa7078a31d9ef5275d481981df52f1b23c51d3
lucialiu <lucialiux3@gmail.com>
2018-05-16 15:18:58 -0400

    fixed Hannah's graph
    I also edited some stylistic changes and also made the first paragraphs shorter.

--- Hannah/project4.js
+++ Hannah/project4.js
@@ -1,30 +1,26 @@
-function tusks(){
-    return "tusks.png";
-}
-document.getElementById("tusks").src = tusks();
 
-var tusk = d3.select("#tusk")
-    .attr("width", 1000)
-    .attr("height", 1000);
+var rectContainer = d3.select("#rectangle-container")
+    .attr("width", 800)
+    .attr("height", 600);
 
-var rectangle = tusk.select("#rect1")
-    .attr("x", 100)
-    .attr("y", 380)
-    .attr("width", 250)
+var rectangle = rectContainer.select("#rect1")      // IVORY
+    .attr("x", 75)
+    .attr("y", 230)
+    .attr("width", 120)
     .attr("height", 10);
 //95,168,244,323, 400, 530
 
-var rectangle1 = tusk.select("#rect2")
-    .attr("x", 900)
-    .attr("y", 380)
-    .attr("width", 250)
+var rectangle1 = rectContainer.select("#rect2")     // WOOD BRANCHES
+    .attr("x", 280)
+    .attr("y", 230)
+    .attr("width", 220)
     .attr("height", 10);
 //10x250, 180x110, 180x250, 320x110, 320x250, 500x110, 500x250
 
-var rectangle2 = tusk.select("#rect3")
-    .attr("x", 450)
-    .attr("y", 380)
-    .attr("width", 360)
+var rectangle2 = rectContainer.select("#rect3")     // ELEPHANTS KILLED
+    .attr("x", 570)
+    .attr("y", 230)
+    .attr("width", 120)
     .attr("height", 10);
 //132, 252, 372, 500
 
@@ -37,12 +33,12 @@ var chess = d3.select("#chess")
 var button = d3.select(".button")
 
 
-piano.on("click", function(){
+piano.on("click", function(){           // don't change the widths!
     piano.style("color", "#614150")
     rectangle.attr("height", 168)
-    rectangle1.attr("width", 250)
+    // rectangle1.attr("width", 250)
     rectangle1.attr("height",180)
-    rectangle2.attr("width", 360)
+    // rectangle2.attr("width", 360)
     rectangle2.attr("height",132)
     
     statues.style("color", "#7d96a0")
@@ -57,9 +53,9 @@ piano.on("click", function(){
 statues.on("click", function(){
     statues.style("color", "#614150")
     rectangle.attr("height", 323) 
-    rectangle1.attr("width", 250)
+    // rectangle1.attr("width", 250)
     rectangle1.attr("height",320)
-    rectangle2.attr("width", 360)
+    // rectangle2.attr("width", 360)
     rectangle2.attr("height",252)
     
     piano.style("color", "#7d96a0")
@@ -75,10 +71,10 @@ statues.on("click", function(){
 wind.on("click", function(){
     wind.style("color", "#614150")
     rectangle.attr("height", 95)
-    rectangle1.attr("width", 110)
+    // rectangle1.attr("width", 110)
     rectangle1.attr("height",180)
     
-    rectangle2.attr("width", 180)
+    // rectangle2.attr("width", 180)
     rectangle2.attr("height",132)
     
     piano.style("color", "#7d96a0")
@@ -93,9 +89,9 @@ wind.on("click", function(){
 jewelry.on("click", function(){
     jewelry.style("color", "#614150")
     rectangle.attr("height", 95)
-    rectangle1.attr("width", 110)
+    // rectangle1.attr("width", 110)
     rectangle1.attr("height",180)
-    rectangle2.attr("width", 180)
+    // rectangle2.attr("width", 180)
     rectangle2.attr("height",132)
     
     piano.style("color", "#7d96a0")
@@ -110,9 +106,9 @@ jewelry.on("click", function(){
 chess.on("click", function(){
     chess.style("color", "#614150")
     rectangle.attr("height", 400) 
-    rectangle1.attr("width", 250)
+    // rectangle1.attr("width", 250)
     rectangle1.attr("height",500)
-    rectangle2.attr("width", 360)
+    // rectangle2.attr("width", 360)
     rectangle2.attr("height",372)
     
     
